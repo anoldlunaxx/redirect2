c1889c27cc87bffa8c7c89d53ac227cb4088806e
anoldlunaxx <160794203+anoldlunaxx@users.noreply.github.com>
2026-09-20 03:46:12 -0500

    Change secret keys for RECAPTCHA and TURNSTILE
    Updated RECAPTCHA and TURNSTILE secret keys.

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask import Flask, request, redirect, send_file, jsonify
 
 app = Flask(__name__)
 
-RECAPTCHA_SECRET = os.environ.get("RECAPTCHA_SECRET", "6LdxAVstAAAAAGa1EJOQ-LanZFulL2rt6fNfmUPU")
-TURNSTILE_SECRET = os.environ.get("TURNSTILE_SECRET", "0x4AAAAAAD48gjQpF8p25655NBEhz4b_bb0")
+RECAPTCHA_SECRET = os.environ.get("RECAPTCHA_SECRET", "6LcdZsUtAAAAACL4f9mpr13ki4Kf5ceF18HKRrxl")
+TURNSTILE_SECRET = os.environ.get("TURNSTILE_SECRET", "0x4AAAAAAE9o-KsUFI2vZlp8EhmFL9hqQTk")
 
 @app.route("/")
 def index():
